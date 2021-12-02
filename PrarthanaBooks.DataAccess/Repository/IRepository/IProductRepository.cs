236ca8e934f32ea2c2ad91fa17814b6bb7ba780c
Prarthana Purav Patil <W0766002@myscc.ca>
2021-12-01 22:44:21 -0500

    Complete Part 3 section 2

---
+++ PrarthanaBooks.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using PrarthanaBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrarthanaBooks.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product product);
+    }
+}
